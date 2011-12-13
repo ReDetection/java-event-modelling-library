a6113de3ec95fd4ceb76943eab3da0d2bcf0c10c
ReDetection <redetection@gmail.com>
2011-12-14 00:07:56 +0700

    simplestopwatch

---
+++ ru/buglakov/study/term7/modelling/jpss/blocks/SimpleStopwatch.java
@@ -0,0 +1,27 @@
+package ru.buglakov.study.term7.modelling.jpss.blocks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.math.BigInteger;
+
+import ru.buglakov.study.term7.modelling.jpss.TimeMachine;
+import ru.buglakov.study.term7.modelling.jpss.Transaction;
+
+/**
+ * Блок, просто засекающий текущее время, когда мимо него проходит транзакт
+ * @author rd
+ */
+public class SimpleStopwatch extends Stat{
+    private List<BigInteger> log = new ArrayList<>();
+    
+    @Override
+    public void receive(Transaction transaction) {
+        log.add(TimeMachine.getTime().getTime());
+        transaction.sendTo(getNext());
+    }
+
+    public List<BigInteger> getLog() {
+        return log;
+    }
+    
+}
