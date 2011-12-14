cf5ea051388c1781e3ad46d8c1b634ce0c2af942
Серега ReDetection <ReDetection@211.ru>
2011-12-14 09:30:36 +0000

    multipoint simplifier

--- ru/buglakov/study/term7/modelling/jpss/Utils.java
+++ ru/buglakov/study/term7/modelling/jpss/Utils.java
@@ -3,10 +3,6 @@ package ru.buglakov.study.term7.modelling.jpss;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-import ru.buglakov.study.term7.modelling.jpss.blocks.Dual;
-import ru.buglakov.study.term7.modelling.jpss.blocks.Multipoint;
-import ru.buglakov.study.term7.modelling.jpss.blocks.TransactionInput;
-import ru.buglakov.study.term7.modelling.jpss.blocks.TransactionOutput;
 
 public class Utils {
 
@@ -16,16 +12,6 @@ public class Utils {
 		a=a.subtract(BigDecimal.ONE);
 		a=a.multiply(new BigDecimal(difference));
 		return average.add(a.toBigInteger());
-	}
-	
-	public static void simplyConnect(Multipoint multi, Dual block, TransactionOutput prev, TransactionInput next){
-		Dual start = multi.createStart();
-		Dual finish = multi.createFinish();
-		
-		prev.setNext(start);
-		start.setNext(block);
-		block.setNext(finish);
-		finish.setNext(next);
 	}
 	
 }
---
+++ ru/buglakov/study/term7/modelling/jpss/blocks/MultipointSimplifier.java
@@ -0,0 +1,28 @@
+package ru.buglakov.study.term7.modelling.jpss.blocks;
+
+import ru.buglakov.study.term7.modelling.jpss.Transaction;
+
+public class MultipointSimplifier extends Dual {
+
+	private Dual start;
+	private Dual finish;
+
+	public MultipointSimplifier(Multipoint multi,Dual contents) {
+		start = multi.createStart();
+		finish = multi.createFinish();
+		
+		start.setNext(contents);
+		contents.setNext(finish);
+	}
+	
+	@Override
+	public void receive(Transaction transaction) {
+		transaction.sendTo(start);
+	}
+	
+	@Override
+	public void setNext(TransactionInput nextBlock) {
+		
+		super.setNext(nextBlock);
+	}
+}
